0c144f03cdd25cb6dbb2f78a8ad2ced44223e9ef
andra2 <andra2016@gmail.com>
2020-02-22 15:35:21 +0300

    V 0.9

--- Laba1/Laba1/Program.cs
+++ Laba1/Laba1/Program.cs
@@ -1,11 +1,12 @@
 ﻿using System;
-using System.Collections;
+using System.Collections.Generic;
 namespace Laba1
 {
     class Program
     {
-        class Commander
+        class Commander                        
         {
+            public List<Minion> army = new List<Minion>();
             private static Commander instance;
             public static Commander GetInstance()
             {
@@ -19,36 +20,87 @@ namespace Laba1
             {
                 
             }
-
-            public void CreateArcher() 
+            public void Construct(int amount,AbstractFactory factory)
             {
-                //todo
+                for (int i = 0; i < amount; i++)
+                {
+                    army.Add(new Minion(factory));
+                }
             }
-            public void CreateSolder() 
+            public void Construct(int amount, AbstractFactory bodyFactory, AbstractFactory weaponFactory)
             {
-                //todo
+                for (int i = 0; i < amount; i++)
+                {
+                    army.Add(new Minion(bodyFactory, weaponFactory));
+                }
             }
+            public void ShowArmy()
+            {
+                int stam=0;
+                int count = 0;
+                int hp = 0;
+                Console.WriteLine("Армия состоит из:");
+                foreach (Minion m in army)
+                {
+                    count++;
+                    stam += m.weapon.s;
+                    hp += m.body.h;
+                    //Console.WriteLine(m.body.name +" c "+ m.weapon.name);
+                }
+                Console.WriteLine("Выносливость армии = {0}\nКоличество войнов = {1}\nЗдоровье армии = {2}", stam.ToString(), count.ToString(), hp.ToString());
+            } 
         }
         class Minion
         {
-            private AbsBody body;
-            private AbsWeapon weapon;
+            public readonly AbsBody body;
+            public readonly AbsWeapon weapon;
 
+            private Minion()
+            {
+                
+            }
+            private Minion(AbsBody b,AbsWeapon w)
+            {
+                body = b;
+                weapon = w;
+            }
             public Minion(AbstractFactory factory)
             {
                 body = factory.CreateBody();
                 weapon = factory.CreateWeapon();
             }
-
+            public Minion(AbstractFactory forBody,AbstractFactory forWeapon)
+            {
+                body = forBody.CreateBody();
+                weapon = forWeapon.CreateWeapon();
+            }
             public void Attack() { Console.WriteLine("атакуем"); }
             public void Wait() { Console.WriteLine("ждем"); }
-            public int health;
-            public int stamina;
+            public Minion Clone()
+            {
+                return new Minion(body,weapon);
+            } 
         }
-        class Sword : AbsWeapon { }
-        class Human : AbsBody { }
-        class Elf : AbsBody { }
-        class Bow : AbsWeapon { }
+        class Sword : AbsWeapon 
+        {
+            new public readonly string n = "меч";
+            new public readonly int s = 15;
+        }
+        class Human : AbsBody 
+        {
+            new public readonly string n= "человек";
+            new public readonly int h = 30;
+        }
+        class Elf : AbsBody 
+        {
+            public readonly string n = "эльф";
+            public readonly int h = 20;
+        }
+        class Bow : AbsWeapon 
+        {
+            public readonly string n = "лук";
+            public readonly int s= 30;
+        }
         abstract class AbstractFactory
         {
             abstract public AbsBody CreateBody();
@@ -56,11 +108,13 @@ namespace Laba1
         }
         abstract class AbsWeapon
         {
-
+            public readonly string n;
+            public readonly int s;
         }
         abstract class AbsBody
         {
-
+            public readonly string n;
+            public readonly int h;
         }
         class Kazarma : AbstractFactory
         {
@@ -73,7 +127,6 @@ namespace Laba1
                 return new Sword();
             }
         }
-
         class ShootingRange : AbstractFactory
         {
             public override AbsBody CreateBody()
@@ -85,9 +138,16 @@ namespace Laba1
                 return new Bow();
             }
         }
+
         static void Main(string[] args)
         {
+            Commander commander = Commander.GetInstance();
+            ShootingRange range = new ShootingRange();
+            Kazarma kazarma = new Kazarma();
 
+            commander.Construct(3,range);
+            commander.Construct(8, kazarma, range);
+            commander.ShowArmy();
         }
     }
 }
